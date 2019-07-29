bd08b27f812d60f8fb65fc48c537dd3423244716
Johan Test <johantest@computercamp.local>
2019-07-29 16:23:35 +0200

    Server

---
+++ server/src/srv/ThreadedEchoServer.java
@@ -0,0 +1,31 @@
+package srv;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ThreadedEchoServer {
+
+    static final int PORT = 1979;
+
+    public static void main(String args[]) {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("I/O error: " + e);
+            }
+            // new thread for a client
+            new EchoThread(socket).start();
+        }
+    }
+}
