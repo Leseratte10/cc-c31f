345fbdd91e56b2d10bbec0d27b669e62102ac122
mobuwer <33159154+mobuwer@users.noreply.github.com>
2019-07-30 12:28:23 +0200

    Thread to receive

--- client/src/client/TCPClient.java
+++ client/src/client/TCPClient.java
@@ -8,19 +8,9 @@ import java.net.Socket;
 
 public class TCPClient {
 	 public static void main(String argv[]) throws Exception {
-	  String sentence;
-	  String modifiedSentence;
-	  boolean fertig = false; 
 	  BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 	  Socket clientSocket = new Socket("172.24.0.42", 1988);
 	  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-	  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-	  while (!fertig) {
-		  	sentence = inFromUser.readLine();
-		  	outToServer.writeBytes(sentence + '\n');
-	  }
-	  modifiedSentence = inFromServer.readLine();
-	  System.out.println("FROM SERVER: " + modifiedSentence);
-	  clientSocket.close(); 
+	  new ThreadReceive(clientSocket).start();
 	 }
 	}
