023dbb08781b818fd88eb7a99e0e9d2698a8a8ff
CampPaul <gfyu@stfu.de>
2019-07-31 12:34:21 +0200

    functional sendfile

--- client/src/client/sendfile.java
+++ client/src/client/sendfile.java
@@ -5,38 +5,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Base64;
 import java.io.*;
-import java.net.URI;
 
 public class sendfile {
-		
-		private static Path path;
-		
-		//public sendfile(URI filepath) {
-			//this.path = Paths.get(filepath);
-		//}
 
-		public static void run() throws IOException {
-			int Count = 0;
-			//this.path = Paths.get(filepath);
-			Path file = Paths.get("H:/C31F/Code Projekt 3.5/Jona Gräf/cc-c31f/TEST.txt");
+		public static String sendfile(String filepath) throws IOException {			
+			Path file = Paths.get(filepath);
 			byte[] fileArray;
 			fileArray = Files.readAllBytes(file);
-			while(true)
-			{
-				System.out.println(fileArray[Count]);
-				Count = Count + 1;
-				if (Count == fileArray.length)
-				{
-					break;
-				}
-			}
+			String encodedfile = Base64.getEncoder().encodeToString(fileArray);
+			String result = "§" + file.getFileName() + "§" + encodedfile + "§";
+			return result;
 		}
-
-		public static Path getPath() {
-			return path;
-		}
-
-		//public void setPath(Path path) {
-			//this.path = path;
-		//}
 }
