604b4db9ea6201d1992e9c64fa613d509e215d39
Karl-Erik Störzel <karlstoer19@computercamp.local>
2019-08-01 11:31:44 +0200

    decode

--- client/src/client/ThreadReceive.java
+++ client/src/client/ThreadReceive.java
@@ -5,10 +5,12 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.net.Socket;
 import java.net.SocketException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
@@ -33,9 +35,17 @@ public class ThreadReceive extends Thread {
         
 		java.util.Date now = new java.util.Date(System.currentTimeMillis());
         String line;
+        BigInteger BigPrivateKey = BigInteger.valueOf(1025);
+        BigInteger BigPublicKey2 = BigInteger.valueOf(2047);
         while (true) {
             try {
                 line = brinp.readLine();
+                System.out.println("Wenn sie decodieren wollen, 1 eingeben ansonsten 0");
+                Scanner sDecode = new Scanner(System.in);
+                int onDecode = sDecode.nextInt();
+                if (onDecode == 1) {
+                line =zahlfinder(line, BigPrivateKey, BigPublicKey2);
+                }
                 System.out.println("<"+sdf.format(now)+">" +" "+line);
             }
             catch(SocketException e) {
@@ -49,6 +59,74 @@ public class ThreadReceive extends Thread {
                     	
         }
 
+	}
+	public static String devigenere (String text) { //vigenere bloﬂ in die andere Richtung bei Verst‰ndnisproblemen, siehe vigenere Kommentare
+		String codewort = "meincodewort";
+		String endwort = "";
+		int j = 0;
+		for (int i = 0; i<text.length(); i++) {
+			if (j==codewort.length()) {
+				j -= codewort.length();
+			}
+			int intBuchstabe =  (int) text.charAt(i);
+			int intCodeBuchstabe = (int) codewort.charAt(j);
+			int newBuchstabe = 0;
+			int addB = intCodeBuchstabe - 96;
+			newBuchstabe = intBuchstabe - addB;
+			if (newBuchstabe < 32) {
+				newBuchstabe += 93; 
+			}
+			System.out.println(newBuchstabe);
+			char ausgabe = (char) newBuchstabe;
+			j +=1;
+			endwort = endwort + ausgabe;
+		}
+		return endwort;
+	}
+	public static String primdecodierung(String zuDecodierendeZahlString, BigInteger privatKey, BigInteger publicKey, String decodierterText) {
+		
+		int zuDecodierendeZahl = Integer.parseInt(zuDecodierendeZahlString);
+		BigInteger BigZuDecodierendeZahl = BigInteger.valueOf(zuDecodierendeZahl);
+		
+		BigInteger decodierteBigZahl = pot (BigZuDecodierendeZahl, BigZuDecodierendeZahl, privatKey, BigInteger.ONE);
+		decodierteBigZahl = decodierteBigZahl.mod(publicKey);
+		int decodierteZahl = decodierteBigZahl.intValue();
+		char decodiert = (char) decodierteZahl;
+		decodierterText = decodierterText + decodiert; 
+		return decodierterText;
+	}
+	public static BigInteger pot(BigInteger zahl, BigInteger ursprungsZahl, BigInteger potenz, BigInteger momentan) {
+		if (potenz.equals(momentan)) {//potenzberechnung beider mit einer biginteger potenz gerechnet wird
+			return zahl;
+		}
+		else {
+			zahl = zahl.multiply(ursprungsZahl);
+			zahl = pot (zahl, ursprungsZahl, potenz, momentan.add(BigInteger.ONE) );
+			
+		}
+		return zahl;
+		
+	}
+	public static String zahlfinder(String text, BigInteger privatKey, BigInteger publicKey) {
+		String decodierterText = "";
+		int zahlenAnzahl = zahlenZaelen(text); //wieviele Zahlen wurden eingegeben?
+		String[] parts = text.split(" "); //text unterteilen in die einzelnen buchstaben indem nach den freizeichen dazwischen gesucht und gespalten wird
+		for (int i = 0; i < zahlenAnzahl ; i++) { //wird auf jeden buchstaben angewendet
+			decodierterText = primdecodierung(parts[i], privatKey, publicKey, decodierterText); //anwendung der decodierung
+		}
+		String ausgabe = devigenere(decodierterText); //vigenere decodierung am Ende
+		return ausgabe;
+		
+	}
+	public static int zahlenZaelen(String text) { //z‰hlt die zahlen aufgrund dessen wie viele freizeichen existieren
+		int anzahl = 1;
+		for(int i = 0; i<text.length();i++) {
+			if (text.charAt(i) == ' ') {
+				anzahl += 1;
+			}
+		}
+		return anzahl;
+		
 	}
 	
 }
