5839d0871f80b7da4db42599956012755ae99ed4
Florian (BE) <>
2019-08-01 17:49:09 +0200

    fix username

--- client/src/client/TCPClient.java
+++ client/src/client/TCPClient.java
@@ -73,7 +73,7 @@ public class TCPClient {
 			System.out.println("Gib deinen Benutzernamen ein! (Windows-Benutzername wird als Default verwendet.)");
 			  Benutzername = inFromUser.readLine();
 			  
-		  if (Benutzername == "" || Benutzername == "	"|| Benutzername == " ") {
+		  if (Benutzername.trim().equals("")) {
 			  String userName = System.getProperty("user.name");
 			  Benutzername = userName;
 		  }
