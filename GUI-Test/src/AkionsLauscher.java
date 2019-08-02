b24ea276f1ab9df9e1a7509a28a692503534a06a
Jannis Stahl <jannstah19@computercamp.local>
2019-08-02 18:11:43 +0200

    GUI-Test

---
+++ GUI-Test/src/AkionsLauscher.java
@@ -0,0 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+public class AkionsLauscher extends Versuch1 implements ActionListener{
+
+
+	public static void main(String args[]) {
+	b =new Button("Drückmich");
+	m1item1.addActionListener(this);
+	}
+}
